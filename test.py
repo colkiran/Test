e9939d1208362cd72367e944c3feed06c31ca876
Kiran Kumar M <colkiran@gmail.com>
2022-01-27 15:36:30 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+#! c:\python39\bin
+
+print("Hello Woorld")
+
+for i in range(!, 6):
+  for k in range(7, i, -1):
+  print(" ", end="")
+  for j in range(1, i + 1):
+    print(j, end = " ")
+print()
+
